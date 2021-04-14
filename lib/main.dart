550a53e7ede7cbd60d24176731d667f07e954d02
Jose Aguilar <jhagui7464@gmail.com>
2021-04-14 17:59:36 -0500

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'screens/fplashScreen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      title: 'EBI Operators',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EBI Operators'),
-        ),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //SystemChrome.setEnabledSystemUIOverlays([]);
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-    ),
-  );
+      home: SplashScreen(),
+    );
+  }
 }
