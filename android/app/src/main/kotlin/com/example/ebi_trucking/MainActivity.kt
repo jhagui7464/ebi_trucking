7711244d285018645b18674e0cc0f2eed9543453
Jose Aguilar <jhagui7464@gmail.com>
2021-04-14 16:19:36 -0500

    start

---
+++ android/app/src/main/kotlin/com/example/ebi_trucking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ebi_trucking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'EBI Operators',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EBI Operators'),
+        ),
+      ),
+    ),
+  );
+}
